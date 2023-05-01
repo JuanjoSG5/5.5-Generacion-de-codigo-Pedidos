a882ed2ba6b0b877ceb2651b6bbff42f0ceeb427
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2023-05-01 08:45:51 +0200

    Implementado codigo base

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import dao.DaoImplementationDB
+import factoriaDatos.DataFactory
+import producto.Product
+import servicios.ProductServImpl
+
+fun main() {
+    // La mayoria del codigo esta basado en el codigo de este enlace: https://github.com/revilofe/UserService
+    // el codigo es practicamente el mismo con algunos cambios como acortar clases, adaptar a la BD H2 ...
+    val dataSource = DataFactory.getDataSource(DataFactory.DataSourceType.Embedded)
+    val daoBD = DaoImplementationDB(dataSource)
+    val userServiceImpl = ProductServImpl(daoBD)
+    userServiceImpl.createTable()
+    val product = Product(1, "Smartphone", 999.99f, "The latest smartphone model",
+        "Apple", "Electronics")
+    val createResult = userServiceImpl.createProduct(product)
+    println(createResult)
+
+    val productId = userServiceImpl.getById(1)?.id
+    println("Product ID: $productId")
+}
